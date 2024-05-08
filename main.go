4f14c18402fed33c678916b59214addf39a5d45e
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-08 13:51:14 -0700

    feat: Initialize repo (exercise 1)

--- main.go
+++ main.go
@@ -38,10 +38,10 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
+		// [DONE]: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/scaffolding",
+		Address: "liatr.io/terraform/devops-bootcamp",
 		Debug:   debug,
 	}
 
